59d58b19ba782558c6161d8b2a1198cd2bce5e52
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2012-05-13 20:38:40 +0200

    exporting DATABASE_URL on vagrant host

--- site-cookbooks/wordpress/recipes/pgsql.rb
+++ site-cookbooks/wordpress/recipes/pgsql.rb
@@ -11,19 +11,31 @@ conn = {:host => "127.0.0.1", :port => 5432, :username => 'postgres',
     :password => node['postgresql']['password']['postgres']}
 
 user = "wordpress"
+pass = "wordpress"
+db = "wordpress"
 
 postgresql_database_user user do
   connection conn
-  password 'wordpress'
+  password pass
   action :create
 end
 
-postgresql_database 'wordpress' do
+postgresql_database db do
   connection conn
   owner user
   action :create
 end
 
+#used by wp-config.php, same env in heroku stack
+template "/etc/profile.d/wordpress.sh" do
+  source "profile_env.erb"
+  owner "root"
+  mode "0644"
+  variables(
+      :env_vars => {"DATABASE_URL" => "postgres://#{user}:#{pass}@localhost:5432/#{db}"}
+  )
+  notifies :reload, resources(:service => "apache2")
+end
 
 apache_user = "vagrant"
 
